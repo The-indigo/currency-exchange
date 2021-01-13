fe5bd1ad827d8f930e0d197058cbeffbbf559d9a
The-indigo <adepojuadeyemi11@gmail.com>
2021-01-13 15:57:54 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+ const express= require('express')
+const bodyParser=require('body-parser')
+const got=require('got');
+const app= express();
+
+
+
+app.use(bodyParser.urlencoded());
+app.use(express.json());
+    
+ app.get('/api/rates',async(req,res)=>{
+    const base=req.query.base;
+    const currency=req.query.currency;
+    try{
+    if(!base || ! currency){
+        return res.json({Error: "Base and currency fields are required"})
+    }
+    const options={
+        url:`https://api.exchangeratesapi.io/latest?base=${base}` ,
+        method:"GET",
+    };
+    const response=await got(options)
+    if(!response)()=> res.json({Error: "There is something wrong with the request,Please Try again"})
+    if(response.statusCode==200){ 
+    const currencyList= currency.split(',')
+    const result=JSON.parse(response.body)
+    let ratesObj= currencyList.reduce(function(acc,curr){
+        acc[curr]=result.rates[curr]
+        return acc;
+    },{})  
+const prototypeObj={
+    result:{
+        base:base,
+        date:result.date,
+        rates:
+          ratesObj
+        
+    }
+};
+
+  return res.json(prototypeObj)
+}
+
+}catch(e){
+    console.log(e)
+}
+ })
+
+
+app.listen( (process.env.PORT || 3000),()=>{
+    console.log("App2 is live");
+})
+
+
+
+    
+
+
+
+
